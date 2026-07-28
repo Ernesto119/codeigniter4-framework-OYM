d6f8ed064bb99cc957e353ea68dfdc4e7ca565f2
gabriel Aragones <Gabriel@gmail.com>
2026-07-28 18:39:41 -0400

    START

---
+++ app/Config/Routes.php
@@ -0,0 +1,14 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/** @var RouteCollection $routes */
+$routes->get('/', 'Home::index');
+$routes->get('/productos', 'Productos::index');
+$routes->get('/productos/crear', 'Productos::crear');
+$routes->post('/productos/guardar', 'Productos::guardar');
+$routes->get('/productos/editar/(:num)', 'Productos::editar/$1');
+$routes->post('/productos/actualizar/(:num)', 'Productos::actualizar/$1');
+$routes->get('/productos/eliminar/(:num)', 'Productos::eliminar/$1');
+$routes->resource('api/productos', ['controller' => 'ApiProductos']);
+//$routes->resource('productos');
